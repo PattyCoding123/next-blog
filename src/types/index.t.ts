1466bba1be4145baa017a1b51a72a5d2b13f140a
Patrick <pducusin123@gmail.com>
2023-05-10 00:32:02 -0400

    Create new NavItem type along with main-nav component file

--- src/types/index.t.ts
+++ src/types/index.t.ts
@@ -1,4 +1,12 @@
+import { type Icon } from "@/components/icons";
+
 export type SiteConfig = {
   title: string;
   description: string;
+};
+
+export type NavItem = {
+  title: string;
+  href: string;
+  icon?: Icon;
 };
