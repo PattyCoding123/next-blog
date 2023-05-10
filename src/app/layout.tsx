0a2d1cb84d67a68674f430bb36850a900045a952
Patrick <pducusin123@gmail.com>
2023-05-09 23:04:13 -0400

    Create root layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,45 @@
+import "@/styles/globals.css";
+import { type Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+export const metadata: Metadata = {
+  title: "My Next Blog",
+  description: "Blog website built with Next.js app directory",
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <>
+      <html lang="en">
+        <body
+          className={cn(
+            "min-h-screen bg-background font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          <div className="relative flex min-h-screen flex-col">
+            <div className="flex-1">{children}</div>
+          </div>
+        </body>
+      </html>
+    </>
+  );
+}
