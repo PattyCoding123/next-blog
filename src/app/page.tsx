863f9bb606b41bc8ac5ef4f4ed52f485cae03f0d
Patrick <pducusin123@gmail.com>
2023-05-10 00:17:16 -0400

    Create basic index page for blog

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="mx-auto px-6">
+      <p className="mb-12 mt-12 text-center text-3xl text-foreground">
+        Hello and Welcome to my Blog!
+      </p>
+    </main>
+  );
+}
