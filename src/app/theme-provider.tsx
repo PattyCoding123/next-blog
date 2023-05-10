c943c6fdff3206510ebb98837dc020951296cfaf
Patrick <pducusin123@gmail.com>
2023-05-09 23:16:49 -0400

    Add comments to clarify use client

--- src/app/theme-provider.tsx
+++ src/app/theme-provider.tsx
@@ -3,6 +3,8 @@
 import { ThemeProvider as NextThemeProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 
+// Allows us to use the Next.js theme provider in our app. Must be
+// a client component.
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemeProvider {...props}>{children}</NextThemeProvider>;
 }
