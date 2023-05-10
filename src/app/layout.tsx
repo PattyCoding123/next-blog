0274cf88b26197cd302275e3a87c0653b2e306e3
Patrick <pducusin123@gmail.com>
2023-05-10 00:25:00 -0400

    Create config type for site config of metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,13 @@ import "@/styles/globals.css";
 import { type Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 
+import { siteConfig } from "@/config/site";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "../components/theme-provider";
 
 export const metadata: Metadata = {
-  title: "My Next Blog",
-  description: "Blog website built with Next.js app directory",
+  title: siteConfig.title,
+  description: siteConfig.description,
   themeColor: [
     { media: "(prefers-color-scheme: light)", color: "white" },
     { media: "(prefers-color-scheme: dark)", color: "black" },
---
+++ src/config/site.ts
@@ -0,0 +1,5 @@
+import { type SiteConfig } from "@/types/index.t";
+export const siteConfig: SiteConfig = {
+  title: "My Next Blog",
+  description: "Blog website built with Next.js app directory",
+};
