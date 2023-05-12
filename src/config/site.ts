7ae21bf6b231d8f21043e10ec165654855e6bb72
Patrick <pducusin123@gmail.com>
2023-05-11 23:33:55 -0400

    Update import for site config

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,4 +1,4 @@
-import { type SiteConfig } from "@/types/index.t";
+import { type SiteConfig } from "@/types";
 export const siteConfig: SiteConfig = {
   title: "My Next Blog",
   description: "Blog website built with Next.js app directory",
