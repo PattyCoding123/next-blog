ec19fce615ceb7045aeacde94b7acceccbabeece
Patrick <pducusin123@gmail.com>
2023-05-14 18:22:17 -0400

    map over posts using the ListItem component

--- src/components/posts.tsx
+++ src/components/posts.tsx
@@ -1,4 +1,5 @@
 import { getSortedPostsData } from "@/lib/posts-data-parser";
+import ListItem from "./list-item";
 
 export default function Posts() {
   // Since the dir is in the root of the project, we don't need to pass any args
@@ -7,7 +8,11 @@ export default function Posts() {
   return (
     <section className="mx-auto mt-6 max-w-2xl">
       <h2 className="text-4xl font-bold text-foreground">Blog</h2>
-      <ul className="w-full">{posts.map((post) => JSON.stringify(post))}</ul>
+      <ul className="w-full">
+        {posts.map((post) => (
+          <ListItem post={post} key={post.id} />
+        ))}
+      </ul>
     </section>
   );
 }
