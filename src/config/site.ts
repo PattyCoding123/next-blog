ed63fbe843885d84246e61552bc3c8ace212d8d2
Patrick <pducusin123@gmail.com>
2023-05-16 15:39:36 -0400

    Change site config

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,5 +1,5 @@
 import { type SiteConfig } from "@/types";
 export const siteConfig: SiteConfig = {
-  title: "My Next Blog",
+  title: "Patrick's Next Blog",
   description: "Blog website built with Next.js app directory",
 };
